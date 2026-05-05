46706faa3eee58aa0a76ce429cfd43a1f2f477e5
Logan Joecks <ljoecks@gmail.com>
2026-05-05 14:23:04 -0400

    fallback to current url after auth

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 
 import { Authenticated, Unauthenticated, useMutation } from "convex/react";
 import { SignInButton, SignUpButton } from "@clerk/clerk-react";
-import { Navigate, Route, Routes } from "react-router";
+import { Navigate, Route, Routes, useLocation } from "react-router";
 import { useEffect } from "react";
 import { api } from "../convex/_generated/api";
 import { Navbar } from "./components/Navbar";
@@ -42,13 +42,16 @@ function UserSync() {
 }
 
 function SignInForm() {
+  const location = useLocation();
+  const fallbackRedirectUrl = `${location.pathname}${location.search}${location.hash}`;
+
   return (
     <div className="flex flex-col gap-8 w-96 mx-auto">
       <p>Log in to see the numbers</p>
-      <SignInButton mode="modal">
+      <SignInButton mode="modal" fallbackRedirectUrl={fallbackRedirectUrl}>
         <button className="btn btn-xl btn-primary">Sign in</button>
       </SignInButton>
-      <SignUpButton mode="modal">
+      <SignUpButton mode="modal" fallbackRedirectUrl={fallbackRedirectUrl}>
         <button className="btn btn-xl btn-soft">Sign up</button>
       </SignUpButton>
     </div>
